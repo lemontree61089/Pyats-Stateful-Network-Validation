1f2e2deeb02e839f997595eabafc5b22eaefeff9
lemontree61089 <pessione.thomas@gmail.com>
2020-10-11 17:57:21 +0200

    Update getState.py

--- getState.py
+++ getState.py
@@ -4,42 +4,46 @@ import os
 import sys
 import socket 
 
-def getIP(hostname): 
-  try:
-    return socket.gethostbyname(hostname)  
-  except: 
-    print("Unable to get IP, might want to double-check the hostname") 
-    exit()
+def get_ip(hostname): 
+    try:
+        return socket.gethostbyname(hostname)  
+    except: 
+       print("Unable to get IP, might want to double-check the hostname") 
+       exit()
 
-def writeTemplate(username,hostname,directory):
-  file_loader = FileSystemLoader('.')
-  env = Environment(loader=file_loader)
-  template = env.get_template('./jinja2/testbed.j2')
-  output = template.render(username=username, hostname=hostname, directory=directory, ip=getIP(hostname))
-  f = open("./testbed.yml", 'w')
-  f.write(output)
-  f.close()
+def write_template(username,hostname,directory):
+    file_loader = FileSystemLoader('.')
+    env = Environment(loader=file_loader)
+    template = env.get_template('./jinja2/testbed.j2')
+    output = template.render(username=username, hostname=hostname, directory=directory, ip=get_ip(hostname))
+    f = open("./testbed.yml", 'w')
+    f.write(output)
+    f.close()
 
-def getModels(argv):
-  i = 4
-  models = ""
-  while i <= len(argv) - 1:
-    models = models+argv[i]+" "
-    i = i + 1
-  return models
+def get_models(argv):
+    i = 4
+    models = ""
+    while i <= len(argv) - 1:
+        models = models+argv[i]+" "
+        i = i + 1
+    return models
 
-if len(sys.argv) < 5:
-  print("Error, usage: ./getState.py username hostname directory model1 model2 ...")
-  print("At least one model has to be provided")
-  exit()
-  
-username = sys.argv[1]
-hostname = sys.argv[2]
-directory = sys.argv[3]
-models = getModels(sys.argv)
+def main(username, hostname, directory, models):
+    write_template(username, hostname, directory)
+    print('template written')
+    print('Launching container to get State')
+    os.system("docker run -it --rm -v /opt/scripts/pyats/:/automation ciscotestautomation/pyats:latest pyats learn "+models+ \
+    "--testbed-file /automation/testbed.yml --output /automation/res/"+directory)
+    print('Done')
 
-writeTemplate(username,hostname,directory)
-
-os.system("docker run -it --rm -v /opt/scripts/pyats/:/automation ciscotestautomation/pyats:latest pyats learn "+models+ \
-"--testbed-file /automation/testbed.yml --output /automation/res/"+directory)
-
+if __name__ == '__main__':
+    if len(sys.argv) < 5:
+        print("Error, usage: ./getState.py username hostname directory model1 model2 ...")
+        print("At least one model has to be provided")
+        exit()
+    else:  
+       username = sys.argv[1]
+       hostname = sys.argv[2]
+       directory = sys.argv[3]
+       models = get_models(sys.argv)
+       main(username, hostname, directory, models)
