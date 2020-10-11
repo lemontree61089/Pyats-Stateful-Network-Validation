04e3ee2ec9326145ee80bbcf9ef19066247d92ad
lemontree61089 <pessione.thomas@gmail.com>
2020-10-11 17:56:51 +0200

    Update diffState.py

--- diffState.py
+++ diffState.py
@@ -2,17 +2,16 @@
 import os
 import sys
 
+def main(directory1, directory2, diff_directory):
+    os.system("docker run -it --rm -v /opt/scripts/pyats/:/automation ciscotestautomation/pyats:latest pyats diff \
+    /automation/res/"+directory1+" /automation/res/"+directory2+" --output /automation/diff/"+diff_directory)
 
-if len(sys.argv) != 4:
-  print("Error, usage: ./getState.py directory1 directory2 diffDirectory")
-  exit()
-  
-directory1 = sys.argv[1]
-directory2 = sys.argv[2]
-diffDirectory = sys.argv[3]
-
-
-os.system("docker run -it --rm -v /opt/scripts/pyats/:/automation ciscotestautomation/pyats:latest pyats diff \
-/automation/res/"+directory1+" /automation/res/"+directory2+" --output /automation/diff/"+diffDirectory)
-
-
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print("Error, usage: ./diffState.py directory1 directory2 diffDirectory")
+        exit()
+    else:
+        directory1 = sys.argv[1]
+        directory2 = sys.argv[2]
+        diff_directory = sys.argv[3]
+        main(directory1, directory2, diff_directory)
